52be8ce5917aafbd78e4b7a127a4ed732116d9f5
Ramp <Ramp@192.168.0.15>
2018-04-13 18:03:44 +0530

    After modify the programme Array_Repeated_Element_Count using one loop.

--- src/com/Logical/Array/Array_Repeated_Element_Count.java
+++ src/com/Logical/Array/Array_Repeated_Element_Count.java
@@ -10,27 +10,22 @@ public class Array_Repeated_Element_Count {
 		int size = input.nextInt();
 
 		int[] crr_array = new int[size];
-		int[] times = new int[size];
+		int[] times = new int[10];
 
 		// Read integers from the console
 		System.out.println("Enter array elements: ");
 		for (int i = 0; i < crr_array.length; i++) {
 			crr_array[i] = input.nextInt();
-			times[i] = 1;
 		}
 
 		// Search for repeated elements
 		for (int j = 0; j < crr_array.length; j++) {
-			for (int i = j; i < crr_array.length; i++) {
-				if (crr_array[j] == crr_array[i] && j != i) {
-					times[i]++;
-				}
-			}
+			times[crr_array[j]] = times[crr_array[j]] + 1;
 		}
 
 		// Printing output
 		for (int i = 0; i < times.length; i++) {
-			System.out.println(crr_array[i] + " occurs " + times[i] + " times");
+			System.out.println(i + " occures " + times[i]);
 		}
 	}
 }
